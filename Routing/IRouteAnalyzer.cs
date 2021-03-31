dee4320a5168904ec2d7aaa1e9f74159b1897426
Khawar Yunus <kyunus@bstglobal.com>
2021-03-31 16:59:36 -0500

    added IRouteAnalyzer interface and a shell implementation

---
+++ Routing/IRouteAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Routing
+{
+    /// <summary>
+    /// Provides routing analyzing features.
+    /// </summary>
+    interface IRouteAnalyzer
+    {
+        /// <summary>
+        /// Given a list of route, it reduces it down to eliminate dependencies.
+        /// </summary>
+        /// <param name="routes"></param>
+        /// <returns></returns>
+        IEnumerable<string> Process(IEnumerable<string> routes);
+    }
+}
---
+++ Routing/RouteAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Routing
+{
+    /// <inheritdoc/>
+    public class RouteAnalyzer : IRouteAnalyzer
+    {
+        /// <inheritdoc/>
+        public IEnumerable<string> Process(IEnumerable<string> routes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
