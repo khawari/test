039adaa3ddb92b4b5aaf89783d58a167bb464b43
Khawar Yunus <kyunus@bstglobal.com>
2021-03-31 20:34:38 -0500

    added unit tests and refactored the code into a separate lib called routinglib

---
+++ RoutingLib/RouteAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace Routing
+{
+    /// <inheritdoc/>
+    public class RouteAnalyzer : IRouteAnalyzer
+    {
+        /// <inheritdoc/>
+        public IEnumerable<string> Process(IEnumerable<string> routes)
+        {
+            var parsedRoutes = routes.Select(i => new Route(i.Split(" -> "))).ToList();
+            List<Route> processedRoutes = new List<Route>();
+            parsedRoutes.ToList().ForEach(parsedRoute =>
+            {
+                // route validation check:
+                // if the input is "2", "1 -> 2" then "1->2" is invalid as per the requirements.
+                if (parsedRoute.Count == 1 && parsedRoutes.Any(i => i != parsedRoute && i.Contains(parsedRoute[0])))
+                    throw new Exception("invalid route.");
+                
+                // if the input is "1->2", "2->"3, "1->3", then "1->3" is invalid per the requirements.
+                if (parsedRoutes.Any(i => i != parsedRoute && i[0] == parsedRoute[0]))
+                    throw new Exception("invalid route.");
+
+                var processedRoute = processedRoutes.SingleOrDefault(i => i.Contains(parsedRoute[0]));
+                if (processedRoute != null)
+                {
+                    processedRoute.Replace(parsedRoute[0], parsedRoute);
+                    processedRoute.CheckForCircularReference();
+                }
+                //else if (parsedRoute.Count() == 2)
+                //{
+                //    processedRoute = processedRoutes.SingleOrDefault(i => i.Contains(parsedRoute[1]));
+                //    if (processedRoute != null)
+                //    {
+                //        processedRoute.Replace(parsedRoute[1], parsedRoute);
+                //        processedRoute.CheckForCircularReference();
+                //    }
+                //    else processedRoutes.Add(parsedRoute);
+                //}
+                else processedRoutes.Add(parsedRoute);
+            });
+
+            return processedRoutes.Select(i => string.Join(" -> ", i.ToList())).ToList();
+        }
+    }
+}
---
+++ RoutingLibTests/RouteAnalyzerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Routing;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+namespace Routing.Tests
+{
+    [TestClass()]
+    public class RouteAnalyzerTests
+    {
+        [TestMethod()]
+        public void ProcessTestHappyPathScenario1()
+        {
+            var analyzer = new RouteAnalyzer();
+            var result = analyzer.Process(new string[] { "1 -> 2", "2 -> 3", "3 -> 4"}).ToList();
+            Assert.IsTrue(result.Count() == 1);
+            Assert.AreEqual(result[0], "1 -> 2 -> 3 -> 4");
+        }
+        [TestMethod()]
+        public void ProcessTestHappyPathScenario2()
+        {
+            var analyzer = new RouteAnalyzer();
+            var result = analyzer.Process(new string[] { "1 -> 2", "2 -> 3", "4", "5 -> 6"}).ToList();
+            Assert.IsTrue(result.Count() == 3);
+            Assert.AreEqual(result[0], "1 -> 2 -> 3");
+            Assert.AreEqual(result[1], "4");
+            Assert.AreEqual(result[2], "5 -> 6");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception), "invalid route.")]
+        public void ProcessTestInvalidRouteScenario1()
+        {
+            var analyzer = new RouteAnalyzer();
+            var result = analyzer.Process(new string[] { "1 -> 2", "1 -> 3", "2 -> 3" }).ToList();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception), "invalid route.")]
+        public void ProcessTestInvalidRouteScenario2()
+        {
+            var analyzer = new RouteAnalyzer();
+            var result = analyzer.Process(new string[] { "1 -> 2", "2", "2 -> 3" }).ToList();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception), "Circular dependency detected")]
+        public void ProcessTestCircularDependencyCheck()
+        {
+            var analyzer = new RouteAnalyzer();
+            var result = analyzer.Process(new string[] { "1 -> 2", "2 -> 3", "3 -> 1" }).ToList();
+        }
+    }
+}
